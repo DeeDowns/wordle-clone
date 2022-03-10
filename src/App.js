b67251290532482bd9fd38f69ad72bace0848e8c
DeeDowns <dee_downs@outlook.com>
2022-03-09 19:52:39 -0800

    add keyboard, handle changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Board from './components/Board'
+import 'react-simple-keyboard/build/css/index.css';
+
+const wordsArr = [
+  new Array(5),
+  new Array(5),
+  new Array(5),
+  new Array(5),
+  new Array(5),
+  new Array(5),
+]
+
+const initialWordContainer = {
+  1: new Array(5),
+  2: new Array(5),
+  3: new Array(5),
+  4: new Array(5),
+  5: new Array(5),
+  6: new Array(5),  
+}
 
 function App() {
+  const [wordNumber, setWordNumber] = useState(1);
+  const [wordContainer, setWordContainer] = useState(initialWordContainer)
+
+
+  const onKeyboardChange = input => {
+    console.log(input)
+    console.log(wordContainer[wordNumber])
+  }
+
+  const onKeyPress = button => {
+    console.log(button)
+    if (button === '{enter}') {
+      setWordNumber(wordNumber + 1)
+    }
+  }
+
+
+  console.log(wordNumber, wordContainer)
   return (
     <div className="App">
       <h1>Wordle Clone</h1>
-      <Board />
+      <Board 
+        onKeyboardChange={onKeyboardChange}
+        onKeyPress={onKeyPress}
+      />
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,16 +1,33 @@
 import Row from './Row'
+import Keyboard from 'react-simple-keyboard';
 
-function Board() {
-  return (
-    <div className="board">
-     <Row/>
-     <Row/> 
-     <Row/> 
-     <Row/> 
-     <Row/> 
-     <Row/> 
-    </div>
-  );
+
+const layout = {
+    default: [
+      "q w e r t y u i o p",
+      "a s d f g h j k l",
+      "{enter} z x c v b n m {bksp}",
+    ],
+}
+function Board(props) {
+    const { onKeyboardChange, onKeyPress } = props;
+
+    return (
+        <div className="board">
+        <Row/>
+        <Row/> 
+        <Row/> 
+        <Row/> 
+        <Row/> 
+        <Row/> 
+        <Keyboard
+            maxLength={5}
+            layout={layout}
+            onChange={onKeyboardChange}
+            onKeyPress={onKeyPress}
+        />
+        </div>
+    );
 }
 
 export default Board;
