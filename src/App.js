6492563082b65155fc019b6351bf39d0ed274c1e
DeeDowns <dee_downs@outlook.com>
2022-03-13 18:28:25 -0700

    create event handlers, pass props

--- src/App.js
+++ src/App.js
@@ -3,49 +3,66 @@ import './App.css';
 import Board from './components/Board'
 import 'react-simple-keyboard/build/css/index.css';
 
-const wordsArr = [
-  new Array(5),
-  new Array(5),
-  new Array(5),
-  new Array(5),
-  new Array(5),
-  new Array(5),
-]
+// const wordsArr = [
+//   new Array(5),
+//   new Array(5),
+//   new Array(5),
+//   new Array(5),
+//   new Array(5),
+//   new Array(5),
+// ]
 
-const initialWordContainer = {
-  1: new Array(5),
-  2: new Array(5),
-  3: new Array(5),
-  4: new Array(5),
-  5: new Array(5),
-  6: new Array(5),  
-}
+// const initialWordContainer = {
+//   1: new Array(5),
+//   2: new Array(5),
+//   3: new Array(5),
+//   4: new Array(5),
+//   5: new Array(5),
+//   6: new Array(5),  
+// }
 
 function App() {
-  const [wordNumber, setWordNumber] = useState(1);
-  const [wordContainer, setWordContainer] = useState(initialWordContainer)
+  const [ guess, setGuess ] = useState(0);
+  const [ wordGuess, setWordGuess ] = useState('');
 
+ 
 
-  const onKeyboardChange = input => {
-    console.log(input)
-    console.log(wordContainer[wordNumber])
+  const onCharSelected = value => {
+    if (wordGuess.length <= 5) {
+      setWordGuess(wordGuess + value)
+    } 
   }
 
-  const onKeyPress = button => {
-    console.log(button)
-    if (button === '{enter}') {
-      setWordNumber(wordNumber + 1)
+  const onBackSpace =  () => {
+    setWordGuess(wordGuess.slice(0, -1))
+  }
+
+  const onWordEnter = () => {
+    console.log('check if wordle',wordGuess)
+  }
+
+  const onKeyClick = event => {
+    const { value } = event.target
+    if (value === 'bksp') {
+      // console.log('delete')
+      onBackSpace()
+    } else if (value === 'enter') {
+      // console.log(wordGuess)
+      onWordEnter()
+    } else {
+      // console.log(value)
+      onCharSelected(value)
     }
   }
 
+  console.log('word',wordGuess)
 
-  console.log(wordNumber, wordContainer)
+
   return (
     <div className="App">
       <h1>Wordle Clone</h1>
       <Board 
-        onKeyboardChange={onKeyboardChange}
-        onKeyPress={onKeyPress}
+        onKeyClick={onKeyClick}
       />
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,33 @@
 import Row from './Row'
-import Keyboard from 'react-simple-keyboard';
+import Keyboard from './Keyboard';
+// import Keyboard from 'react-simple-keyboard';
 
 
-const layout = {
-    default: [
-      "q w e r t y u i o p",
-      "a s d f g h j k l",
-      "{enter} z x c v b n m {bksp}",
-    ],
-}
+// const layout = {
+//     default: [
+//       "q w e r t y u i o p",
+//       "a s d f g h j k l",
+//       "{enter} z x c v b n m {bksp}",
+//     ],
+// }
 function Board(props) {
-    const { onKeyboardChange, onKeyPress } = props;
+    const { onKeyClick } = props;
 
     return (
         <div className="board">
-        <Row/>
-        <Row/> 
-        <Row/> 
-        <Row/> 
-        <Row/> 
-        <Row/> 
-        <Keyboard
-            maxLength={5}
-            layout={layout}
-            onChange={onKeyboardChange}
-            onKeyPress={onKeyPress}
-        />
+            <Row/>
+            <Row/> 
+            <Row/> 
+            <Row/> 
+            <Row/> 
+            <Row/> 
+            <Keyboard onKeyClick={onKeyClick}/>
+            {/* <Keyboard
+                maxLength={5}
+                layout={layout}
+                onChange={onKeyboardChange}
+                onKeyPress={onKeyPress}
+            /> */}
         </div>
     );
 }
